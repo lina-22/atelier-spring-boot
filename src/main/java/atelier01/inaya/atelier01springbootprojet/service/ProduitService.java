4a13f02bec9fa6ebfde9059ad41e9f14a6e719e1
mustfahinur <mustfahinur@gmail.com>
2023-11-07 10:28:57 +0100

    package service init

---
+++ src/main/java/atelier01/inaya/atelier01springbootprojet/service/ProduitService.java
@@ -0,0 +1,14 @@
+package atelier01.inaya.atelier01springbootprojet.service;
+
+import atelier01.inaya.atelier01springbootprojet.Produit;
+
+import java.util.List;
+
+public interface ProduitService {
+    Produit saveProduit(Produit p);
+    Produit updateProduit(Produit p);
+    void deleteProduit(Produit p);
+    void deleteProduitById(Long id);
+    Produit getProduit(Long id);
+    List<Produit> getAllProduits();
+}
