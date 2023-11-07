e94ede9a5a152d3709926da1aecd78d268173f50
mustfahinur <mustfahinur@gmail.com>
2023-11-07 10:04:56 +0100

    first init

---
+++ src/main/java/atelier01/inaya/atelier01springbootprojet/Atelier01springBootProjetApplication.java
@@ -0,0 +1,13 @@
+package atelier01.inaya.atelier01springbootprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atelier01springBootProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atelier01springBootProjetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/atelier01/inaya/atelier01springbootprojet/Atelier01springBootProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package atelier01.inaya.atelier01springbootprojet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atelier01springBootProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
